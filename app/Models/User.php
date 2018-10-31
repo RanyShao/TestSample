94d471b039deac6f7edd27685650bb1ea2f07ba9
Rany <shao896105226@163.com>
2018-10-31 16:48:38 +0800

    'add'

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,6 +33,6 @@ class User extends Authenticatable
     public function gravetar($size = '99')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
-        return 'http://www.gravatar.com/avatar/$hash?s=$size';
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
 }
